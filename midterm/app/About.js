e4423eb232968b02eef8c0a756524b54f51cee1c
LimHaeJun <6462jun@gmail.com>
2023-10-18 13:37:18 +0900

    third commit

--- midterm/app/About.js
+++ midterm/app/About.js
@@ -2,7 +2,6 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Navbar from "../components/Navbar";
 
-export default function Home() {
-  <Navbar />;
-  return <p>page.js 201730129 임해준</p>;
+export default function About() {
+  return <p>About.js 201730129 임해준</p>;
 }
---
+++ midterm/app/Contact.js
@@ -0,0 +1,6 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Contact() {
+  return <p>Contact.js 201730129 임해준</p>;
+}
